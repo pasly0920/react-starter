8ee5019ae0d75c7cf13a74a31544becca90057d9
pasly0920 <inwoo920621@gmail.com>
2022-01-01 23:00:02 +0900

    useEffect cleanup Function Pracice

--- src/App.js
+++ src/App.js
@@ -3,30 +3,21 @@ import Button from "./Button.js";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    if (keyword.length > 5) console.log("SEARCH FOR", keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log("counter and keyword changes");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click Me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
